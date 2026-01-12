b3ff70b0eee78db6ee993c806766fbe677e92e37
amani-patrick <pazzoamani@gmail.com>
2026-01-12 11:50:42 +0200

    test data of cloud risk scanner

---
+++ cloud-risk-scanner/samples/vulnerable.tf
@@ -0,0 +1,20 @@
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from anywhere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
